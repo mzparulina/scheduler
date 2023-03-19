13a2c1189ab371d7302c40fc2c693127b83195e0
mzparulina <pauline.revilla@gmail.com>
2023-03-19 15:15:43 -0400

    Refactor InterviewerListItem.js to hide name if it's not selected

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -8,13 +8,13 @@ export default function InterviewerListItem(props) {
   });
 
   return (
-    <li id={props.id} className={interviewersItem} onClick={() => props.setInterviewer(props.id)}>
+    <li className={interviewersItem} onClick={() => props.setInterviewer(props.id)}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-      {props.name}
+      {props.selected && props.name}
     </li>
   );
 }
