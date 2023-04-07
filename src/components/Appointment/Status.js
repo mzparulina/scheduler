f64c8f3489295c2928095ab6ffbeceb4ac40e528
mzparulina <pauline.revilla@gmail.com>
2023-04-07 09:10:34 -0400

    Refactor Status component to receive message props

--- src/components/Appointment/Status.js
+++ src/components/Appointment/Status.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 export default function Status(props) {
+  const msg = props.message;
+
   return (
     <main className="appointment__card appointment__card--status">
       <img
@@ -8,7 +10,7 @@ export default function Status(props) {
         src="images/status.png"
         alt="Loading"
       />
-      <h1 className="text--semi-bold">Deleting</h1>
+      <h1 className="text--semi-bold">{msg}</h1>
     </main>
   );
 }
