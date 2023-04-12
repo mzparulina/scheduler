b9b94661539c1edecb0d814fb05455669b99cfe1
mzparulina <pauline.revilla@gmail.com>
2023-04-12 17:16:32 -0400

    Remove unnecessary test on Appointment

--- src/components/__tests__/Appointment.js
+++ src/components/__tests__/Appointment.js
@@ -8,12 +8,4 @@ describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Appointment />);
   });
-
-  it("does something it is supposed to do", () => {
-    // ...
-  });
-
-  it("does something else it is supposed to do", () => {
-    // ...
-  });
 });
