9ccbd9dfac1021a22222048fe4ca0070d602823d
mzparulina <pauline.revilla@gmail.com>
2023-04-14 19:35:12 -0400

    add tests loads data

--- src/components/__tests__/Application.js
+++ src/components/__tests__/Application.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
-import { fireEvent, waitForElement } from "@testing-library/react";
-import { act } from 'react-dom/test-utils';
+import { fireEvent, waitForElement, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
 import Application from "components/Application";
 
 afterEach(cleanup);
@@ -14,5 +13,26 @@ describe("Application", () => {
       fireEvent.click(getByText("Tuesday"));
       expect(getByText("Leopold Silvers")).toBeInTheDocument();
     });
+  });
+
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Save"));
+
+    console.log(prettyDOM(appointment));
+
   });
 });
